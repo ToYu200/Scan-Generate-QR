142a314c71ae03918cc6ddbf00fb420211aa330a
Анатолий <subota300716@gmail.com>
2025-03-17 23:03:04 +0300

    Подключил другой сканнер

--- src/components/Scan/QrCodeScanner.jsx
+++ src/components/Scan/QrCodeScanner.jsx
@@ -1,46 +1,37 @@
-import { Scanner } from '@yudiel/react-qr-scanner';
 import { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
 import s from './qrCodeScanner.module.css';
 import { SCAN_DATA } from '../../constants';
 
-const QrCodeScanner = () => {
-
+export const QrCodeScanner = () => {
     const [scanned, setScanned] = useState(null);
 
-    const scanHandeler = (result) => {
-        setScanned(result[0].rawValue);
+    const scanHandler = (result) => {
         if (!result) return;
 
         const prevData = JSON.parse(localStorage.getItem(SCAN_DATA) || '[]');
 
         if (prevData.includes(result.text)) return;
 
+        setScanned(result.text);
+
         localStorage.setItem(
             SCAN_DATA,
             JSON.stringify([...prevData, result.text])
         );
+    };
 
-    }
-
-    const settings = {
-        audio: false,
-        finder: false,
-    }
-
-    const stylesSettings = {
-        container: {
-        width: 350,
-        }
-    }
-
-    return <div className={s.container}>
-        <p>Отсканируйте QR код</p>
-        <Scanner components={settings} allowMultiple={true} onScan={scanHandeler} styles={stylesSettings} />
-        <a href={scanned} className={s.text}>{scanned}</a>
+    return (
+        <div className={s.container}>
+            <p>Отсканируйте QR-код</p>
+            <QrReader
+                constraints={{ facingMode: 'environment' }}
+                scanDelay={1000}
+                onResult={scanHandler}
+                containerStyle={{ width: '300px' }}
+            />
+    
+            <p className={s.result}>{scanned}</p>
         </div>
-
-
+    );
 };
-
-
-export { QrCodeScanner };
